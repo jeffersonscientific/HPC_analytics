de06a1efbd527075a2c1479ab00d16e5d8cc9668
Mark Yoder <mark.yoder@gmail.com>
2020-02-10 09:59:02 -0800

    adding mazama usage analytics script.

---
+++ get_usage_mazama.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -o sacct_mazama_out.out
+#SBATCH -e sacct_mazama_out.err
+
+# can we pass "\t" correctly? maybe for now, just use the HPC standard "|" as a delimiter...
+STARTTIME="2019-01-01"
+#FORMAT="User,Group,GID,JobID,Jobname,partition,state,Submit,time,Eligible,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,SystemCPU,UserCPU,TotalCPU,Suspended"
+FORMAT="ALL"
+#
+#srun sacct -a -p --delimiter="|" --${STARTTIME} --format=User,Group,GID,JobID,Jobname,partition,state,Submit,time,Eligible,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,SystemCPU,UserCPU,TotalCPU,Suspended
+
+srun sacct -a -p --delimiter="|" --${STARTTIME} --format=${FORMAT}
+
