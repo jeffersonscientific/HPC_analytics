f2fc02c9ee4df22e82b5c619dd912961e72f3916
Mark Yoder <mark.yoder@gmail.com>
2020-02-11 15:22:49 -0800

    mods to sherlock HPC scacct date range

--- get_usage_sherlock_hns.sh
+++ get_usage_sherlock_hns.sh
@@ -4,7 +4,8 @@
 #SBATCH -e sacct_mazama_out.err
 
 # can we pass "\t" correctly? maybe for now, just use the HPC standard "|" as a delimiter...
-STARTTIME="2019-01-01"
+STARTTIME="2020-01-01"
+ENDTIME="2020-1-31"
 #FORMAT="User,Group,GID,JobID,Jobname,partition,state,Submit,time,Eligible,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,SystemCPU,UserCPU,TotalCPU,Suspended"
 FORMAT="ALL"
 #
