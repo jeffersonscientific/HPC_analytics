7be83102c51786ef0d2ff92b0bf729df6dec0ffa
Mark <mark.yoder@gmail.com>
2020-02-11 11:24:16 -0800

    synching with remotes. correcting starttime error in sherlock-hns script. ;

--- get_usage_sherlock_hns.sh
+++ get_usage_sherlock_hns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -o sacct_mazama_out.out
-#SBATCH -e sacct_mazama_out.err
+#SBATCH -o sacct_sherlock_out.out
+#SBATCH -e sacct_sherlock_out.err
 
 # can we pass "\t" correctly? maybe for now, just use the HPC standard "|" as a delimiter...
 STARTTIME="2019-01-01"
@@ -10,5 +10,5 @@ FORMAT="ALL"
 #
 #srun sacct -a -p --delimiter="|" --${STARTTIME} --format=User,Group,GID,JobID,Jobname,partition,state,Submit,time,Eligible,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,SystemCPU,UserCPU,TotalCPU,Suspended
 
-srun sacct --allusers --partition=hns -p --delimiter="|" --${STARTTIME} --format=${FORMAT}
+srun sacct --allusers --partition=hns -p --delimiter="|" ----starttime=${STARTTIME} --format=${FORMAT}
 
