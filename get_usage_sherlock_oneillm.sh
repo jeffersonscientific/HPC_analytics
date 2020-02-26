a65e96459508b4676b58a2ec204c9a70bbfc7377
Mark <mark.yoder@gmail.com>
2020-02-26 08:07:34 -0800

    merging with remote.

---
+++ get_usage_sherlock_oneillm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -o sacct_sherlock_out_oneillm.out
+#SBATCH -e sacct_sherlock_out_oneillm.err
+#SBATCH -p normal
+#
+# can we pass "\t" correctly? maybe for now, just use the HPC standard "|" as a delimiter...
+STARTTIME="2019-11-01"
+ENDTIME="2020-11-30"
+FORMAT="User,Group,GID,JobID,Jobname,partition,state,Submit,time,Eligible,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,SystemCPU,UserCPU,TotalCPU,Suspended"
+#FORMAT="ALL"
+PARTITION="oneillm"
+#
+#srun sacct -a -p --delimiter="|" --${STARTTIME} --format=User,Group,GID,JobID,Jobname,partition,state,Submit,time,Eligible,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,SystemCPU,UserCPU,TotalCPU,Suspended
+
+srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2019-08-16 --endtime=2019-09-15 --format=${FORMAT}
+srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2019-09-16 --endtime=2019-10-15 --format=${FORMAT}
+srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2019-10-16 --endtime=2019-11-15 --format=${FORMAT}
+srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2019-11-16 --endtime=2019-12-15 --format=${FORMAT}
+srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2019-12-16 --endtime=2020-01-15 --format=${FORMAT}
+srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2020-01-16 --endtime=2020-02-15 --format=${FORMAT}
+#srun sacct --allusers --partition=$PARTITION -p --delimiter="|" --starttime=2020-01-16 --endtime=2020-02-15 --format=${FORMAT}
+
+#srun sacct --allusers --partition=hns -p --delimiter="|" --starttime=${STARTTIME} --endtime=${ENDTIME} --format=${FORMAT}
+# sacct --allusers --starttime=2020-02-01 --endtime=2020-02-29 --partition=hns
+
