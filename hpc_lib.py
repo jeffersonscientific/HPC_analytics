28d7bfd9f95a517b10f33c31c932d072f57552a2
Mark Yoder <mark.yoder@gmail.com>
2020-02-27 15:42:04 -0800

    more development bits in Mazama_usage. moving salient bits to class structure.

---
+++ hpc_lib.py
@@ -0,0 +1,48 @@
+# functions, classes, and other helper bits for HPC_analytics notebooks and other libs.
+#
+import numpy
+import scipy
+#import matplotlib
+import matplotlib.dates as mpd
+#import pylab as plt
+import datetime as dtm
+#
+#import pandas
+#
+def str2date(dt_str, verbose=0):
+    try:
+        return mpd.num2date(mpd.datestr2num(dt_str))
+    except:
+        if verbose:
+            print('error date conversion: ', dt_str)
+            #raise Exception('date conversion exception')
+        #
+        return None
+
+#
+def elapsed_time_2_sec(tm_in):
+    # guessing for now...
+    tm_ins = tm_in.split('-')
+    #
+    if len(tm_ins)==0:
+        return 0.
+    if len(tm_ins)==1:
+        days=0
+        tm=tm_ins[0]
+    if len(tm_ins)>1:
+        days = tm_ins[0]
+        tm = tm_ins[1]
+    #
+    h,m,s = tm.split(':')
+    #try:
+    #    h,m,s = tm.split(':')
+    #except:
+    #    print('*** AHHH!!! error! ', tm, tm_in)
+    #    raise Exception("broke on elapsed time.")
+        
+    #
+    return float(days)*24.*3600. + float(h)*3600. + float(m)*60. + float(s)
+#
+def running_mean(X, n=10):
+    return (numpy.cumsum(X)[n:] - numpy.cumsum(X)[:-n])/n
+#
